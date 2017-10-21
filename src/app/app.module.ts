1e97f37155be4015fa16eab927a7d0dd8581ca1d
williamsjs <williamsjs1224@gmail.com>
2017-10-21 11:41:52 -0400

    move core into its own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { BooksService } from './shared/services/books.service';
 import { AuthorsService } from './shared/services/authors.service';
-import { CoreService } from './core.service';
+import { CoreService } from './core/core.service';
 
 import { AppComponent } from './app.component';
 import { BookSearchInputComponent } from './books/book-search-input/book-search-input.component';
