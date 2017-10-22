7cc9f4975497aca455af7e6651b8c7e858d03f94
williamsjs <williamsjs1224@gmail.com>
2017-10-21 20:38:24 -0400

    move search list search input to shared folder

---
+++ src/app/shared/search-list/search-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-search-list',
+  templateUrl: './search-list.component.html',
+  styleUrls: ['./search-list.component.scss']
+})
+export class SearchListComponent  {
+  @Input() items: string;
+}
