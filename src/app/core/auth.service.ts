a35ef634e4f03f68f80519911a589f1f07e64975
williamsjs <williamsjs1224@gmail.com>
2017-11-02 20:34:50 -0400

    add jwt helper service to auth service

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 import { CoreService } from './core.service';
 
@@ -8,9 +9,11 @@ import { CoreService } from './core.service';
 export class AuthService {
   private apiUrl: string;
 
-  constructor(private http: HttpClient, private coreService: CoreService) {
-    this.apiUrl = coreService.API_URL;
-  }
+  constructor(private http: HttpClient,
+              private coreService: CoreService,
+              private jwtHelperService: JwtHelperService) {
+                  this.apiUrl = coreService.API_URL;
+              }
 
   authenticate(email: string, password: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/authenticate`, {
