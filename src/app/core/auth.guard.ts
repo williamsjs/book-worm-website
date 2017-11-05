d0f99228cf0570feec545098eaaa5c9f90f6e70d
williamsjs <williamsjs1224@gmail.com>
2017-11-05 10:34:54 -0500

    add redirect from guard

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+    this.router.navigate(['/login']);
+    return false;
   }
 }
