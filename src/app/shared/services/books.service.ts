c8a1864d29bebe526381a36ec6c9e47386a05485
williamsjs <williamsjs1224@gmail.com>
2017-11-11 11:10:53 -0500

    add auth header to request

--- src/app/shared/services/books.service.ts
+++ src/app/shared/services/books.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { CoreService } from '../../core/core.service';
+import { AuthService } from '../../core/auth.service';
 
 @Injectable()
 export class BooksService {
   private apiUrl: string;
 
-  constructor(private http: HttpClient, private coreService: CoreService) {
+  constructor(private http: HttpClient, private coreService: CoreService, private authService: AuthService) {
     this.apiUrl = coreService.API_URL;
   }
 
   getBooks(title: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/books?title=${title}`);
+    return this.http.get<any[]>(`${this.apiUrl}/books?title=${title}`,
+      {headers: new HttpHeaders().set('Authorization', this.authService.getToken())});
   }
 
   getBook(bookId: number): Observable<any> {
