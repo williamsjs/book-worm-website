dd1f86239efea7677f2a18758c42a9ccd638287c
williamsjs <williamsjs1224@gmail.com>
2017-11-13 18:30:06 -0500

    refactor search input component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 import { Router } from '@angular/router';
 import { BooksService } from '../shared/services/books.service';
 import { AuthService } from '../core/auth.service';
 import { BookItem } from '../shared/models/book-item';
 import { ISearchItem } from '../shared/interfaces/search-item.interface';
 
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   public books: BookItem[];
+  private searchTerm = new Subject<string>();
 
   constructor(private booksService: BooksService,
               private authService: AuthService,
               private router: Router) {}
 
-  listBooks(searchInput: string): void {
+  ngOnInit() {
+    this.searchTerm
+        .distinctUntilChanged()
+        .debounceTime(300)
+        .subscribe(term => this.displayBooks(term));
+  }
+
+  displayBooks(searchInput: string): void {
     this.booksService.getBooks(searchInput)
       .subscribe(books => {
         this.books = books.map(book => this.createBook(book));
     });
+  }
+
+  listBooks(searchInput: string): void {
+    this.searchTerm.next(searchInput);
   }
 
   createBook(book: any): BookItem {
--- src/app/shared/search-input/search-input.component.ts
+++ src/app/shared/search-input/search-input.component.ts
@@ -1,31 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.scss']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent {
   @Output() inputChange: EventEmitter<string> = new EventEmitter<string>();
-  private searchTerm = new Subject<string>();
-
-  ngOnInit() {
-    this.searchTerm
-      .debounceTime(300)
-      .distinctUntilChanged()
-      .subscribe(term => this.inputChange.emit(term));
-  }
 
   searchInputChange(searchTerm: string): void {
     if (searchTerm) {
-      this.searchTerm.next(searchTerm);
+      this.inputChange.emit(searchTerm);
     }
   }
 
